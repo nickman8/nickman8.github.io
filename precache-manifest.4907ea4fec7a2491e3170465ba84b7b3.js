e6888b8fff559f643af5dbbe77c3de7065343452
nickman8 <57720318+nickman8@users.noreply.github.com>
2020-09-23 18:43:35 +0300

    add chat

---
+++ precache-manifest.4907ea4fec7a2491e3170465ba84b7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f483c6c748a9806793976a60c45638ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4aeb5c1803d07c7622",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "573fb0c8318a08116b51",
+    "url": "/static/js/2.ac7d663d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ac7d663d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4aeb5c1803d07c7622",
+    "url": "/static/js/main.3b6fcc7f.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  }
+]);
